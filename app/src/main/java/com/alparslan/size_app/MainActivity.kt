9a4f124fbae1f56c8c4204398dbd85640eeb8187
origin <https://github.com/alparslanozden/Kotlin_Notes>
2022-03-18 23:58:41 +0300

    Font Size Changer App

---
+++ app/src/main/java/com/alparslan/size_app/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.alparslan.size_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val initialTextViewTranslationY = textView_progress.translationY
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView_progress.text = progress.toString()
+
+                val translationDistance = (initialTextViewTranslationY +
+                        progress * resources.getDimension(R.dimen.text_anim_step) * -1)
+
+                textView_progress.animate().translationY(translationDistance)
+                if (!fromUser)
+                    textView_progress.animate().setDuration(500).rotationBy(360f)
+                        .translationY(initialTextViewTranslationY)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        button_reset
+        button_reset.setOnClickListener { v ->
+            seekBar.progress = 0
+        }
+    }
+}
+    }
+}
